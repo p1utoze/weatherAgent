c204c59e0ccfbb55ab65616598f96a9a6f1b15cf
Adithya Awati <aditya.n.awati@gmail.com>
2023-10-10 21:48:20 +0530

    Update temperature.py

--- src/messages/temperature.py
+++ src/messages/temperature.py
@@ -1,24 +1,24 @@
-from uagents import Agent, Protocol, Model, Context
-from src.utils.api import fetch_realtime_api
+from uagents import Model
 
 
 class UserValues(Model):
+    """
+    /limit POST request body parameters
+    """
     min_value: int
     max_value: int
     location: str
 
 
 class CurrentTemperature(Model):
+    """
+    Message Passing Model from weather agent to temp_monitor agent
+    """
     value: float
 
 
 class WeatherFields(Model):
+    """
+    Weather API response fields passed from temp_monitor agent to weather agent
+    """
     LOCATION: str
-
-
-class Alert(Model):
-    message: str
-
-
-
-
